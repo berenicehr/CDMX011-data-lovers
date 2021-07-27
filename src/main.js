22a6652434594575ed8c866a4bdeaecb9a28e80a
Andrea Herrera <ands.he09@gmail.com>
2021-07-27 11:51:46 -0500

    mostrar data forEach

--- src/main.js
+++ src/main.js
@@ -3,4 +3,22 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+// eslint-disable-next-line no-console
 console.log(example, data);
+const characters = data.results
+let characterHtml = ""
+characters.forEach((character=> {
+  characterHtml += `<div class= "card">
+           <img src = "${character.image}"> 
+          <div class="info">   
+          <h3>${character.name} </h3>
+          <p>Status: ${character.status}</p>
+          <p>Specie: ${character.species}</p>
+          <p>Gender: ${character.gender}</p>
+          <p>Type: ${character.type}</p>
+          <p>Origin: ${character.origin.name}</p>
+      
+        </div>
+       </div> `
+}))
+document.getElementById("data").innerHTML = characterHtml
